bccf720f203c275cf15826e748bfddc6e3760934
Muhammadjunaid7864 <muhammadjunaid104developer@gmail.com>
2025-06-23 15:59:04 +0500

    added kv model

--- kv_service/app/kv/models.py
+++ kv_service/app/kv/models.py
@@ -1,3 +1,23 @@
 from django.db import models
-
+from datetime import datetime
 # Create your models here.
+class KvSecretPath(models.Model):
+    path        = models.CharField(max_length=255, unique=True)
+
+    def __str__(self):
+        return self.path
+
+class KvSecretVersion(models.Model):
+    secret_path = models.ForeignKey(KvSecretPath, on_delete=models.CASCADE)
+    data        = models.JSONField()
+    version     = models.IntegerField()
+    created_at  = models.DateTimeField(default=datetime.now())
+    deleted     = models.BooleanField(default=False)
+
+
+    class Meta:
+        unique_together = ("secret_path","version")
+        ordering        = ["-version"]
+        indexes         = [
+            models.Index(fields=["secret_path"]),
+        ]
